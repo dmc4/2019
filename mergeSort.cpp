797c3da56743602c125cfdb7e88339854e7dc875
Changze Huang <1151042040@qq.com>
2019-12-26 23:43:57 +0800

    Create mergeSort.cpp

---
+++ mergeSort.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mergeSort
+//
+//  Created by Toby on 26/12/19.
+//  Copyright © 2019 Toby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int size;
+    std::cout << "Give the size of array";
+    return 0;
+}
